045e7092ceb346e53bef8b9c34dbb0d9df108898
Bogdan B Peterson <bogdan.bryzh@gmail.com>
2022-11-16 17:19:28 +0200

    feat: added sending image

--- apps/api/src/app/controllers/apartments.controller.ts
+++ apps/api/src/app/controllers/apartments.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { donwloadFileFromS3 } from '@s3-photo-download/utils/storage';
+import { PassThrough } from 'stream';
 
 export const createApartment = (request: Request, response: Response) => {
   response.status(200).json({
@@ -19,14 +20,18 @@ export const getApartment = async (request: Request, response: Response) => {
     Key: '97bea3d33f21fba2b84dc8f0df91bb3e',
   };
 
-  const imageFile = await donwloadFileFromS3(image);
+  const imageFile = await (
+    await donwloadFileFromS3(image)
+  ).Body.transformToByteArray();
 
-  console.log(`result`);
-  console.log(JSON.stringify(imageFile, null, 2));
+  const stream = new PassThrough();
 
-  response.status(200).json({
-    message: 'ok',
-  });
+  stream.end(imageFile);
+  response.set('Content-Disposition', `attachment; filename=${image.Key}.png`);
+  response.set('Content-Type', 'image/png');
+  stream.pipe(response);
+
+  response.status(200);
 };
 
 export const updateApartment = (request: Request, response: Response) => {
